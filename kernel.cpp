074c0c271efc1f8bfcf8f3f720e8acb859f94a0d
ruchisahu <sahruchi@gmail.com>
2018-06-15 11:23:33 -0700

    memory management

---
+++ kernel.cpp
@@ -0,0 +1,20 @@
+
+#include "types.h"
+#include "gdt.h"
+void printf(char* str)
+{
+	static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+	
+	for(int i = 0; str[i] != '\0'; ++i)
+		 VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+ extern "C" void kernelMain(void* multiboot_structure,uint32_t /*multiboot_magic*/)
+ {
+	
+    printf("My First Kernel\n");
+	GlobalDescriptorTable gdt;
+	while(1);
+  
+    
+  }
